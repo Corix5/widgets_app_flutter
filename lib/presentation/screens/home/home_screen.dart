de0b71cd09fc83feed038e18f5dec577f88f2356
DCortes-Tedrak <david.cortes@tedrak.com.mx>
2025-06-05 11:45:42 -0600

    rutas con nombre implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/theme/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -45,6 +46,7 @@ class _CustomListTile extends StatelessWidget {
       subtitle: Text(menuItem.subtitle),
       onTap: () {
         //Navigator.pushNamed(context, menuItem.link);
+        //context.pushNamed(CardsScreen.name);
         context.push(menuItem.link);
       },
     );
