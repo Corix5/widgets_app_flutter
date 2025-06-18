bc6cf5d2cad5b8a9bfd59dd06dadbe41a0e6b38d
DCortes-Tedrak <david.cortes@tedrak.com.mx>
2025-06-17 18:38:21 -0600

    inicio de riverpod

--- lib/config/theme/menu/menu_items.dart
+++ lib/config/theme/menu/menu_items.dart
@@ -6,57 +6,68 @@ class MenuItems {
   final String link;
   final IconData icon;
 
-  const MenuItems({required this.title, required this.subtitle, required this.link, required this.icon});
+  const MenuItems({
+    required this.title,
+    required this.subtitle,
+    required this.link,
+    required this.icon,
+  });
 }
 
- const appMenuItems = <MenuItems>[
+const appMenuItems = <MenuItems>[
+  MenuItems(
+    title: 'Counter Riverpod',
+    subtitle: 'Contador de Riverpod',
+    link: '/counter',
+    icon: Icons.add,
+  ),
+
   MenuItems(
     title: 'Botones',
     subtitle: 'Varios botones en Flutter',
     link: '/buttons',
-    icon: Icons.smart_button_outlined
+    icon: Icons.smart_button_outlined,
   ),
   MenuItems(
     title: 'Tarjetas',
     subtitle: 'Un contador estilizado',
     link: '/cards',
-    icon: Icons.credit_card
+    icon: Icons.credit_card,
   ),
   MenuItems(
     title: 'Progress Indicators',
     subtitle: 'Generales y controlados',
     link: '/progress',
-    icon: Icons.refresh_rounded
+    icon: Icons.refresh_rounded,
   ),
   MenuItems(
     title: 'Snackbars y diálogos',
     subtitle: 'Indicadores en pantalla',
     link: '/snackbars',
-    icon: Icons.info_outline
+    icon: Icons.info_outline,
   ),
   MenuItems(
     title: 'Animated container',
     subtitle: 'Stateful widget animado',
     link: '/animated',
-    icon: Icons.check_box_outlined
+    icon: Icons.check_box_outlined,
   ),
   MenuItems(
     title: 'UI Controls + Tiles',
     subtitle: 'Una serie de controles de Flutter',
     link: '/ui-controls',
-    icon: Icons.car_rental_outlined
+    icon: Icons.car_rental_outlined,
   ),
   MenuItems(
     title: 'Introducción a la aplicación',
     subtitle: 'Pequeño tutorial introductorio',
     link: '/tutorial',
-    icon: Icons.accessibility_rounded
+    icon: Icons.accessibility_rounded,
   ),
   MenuItems(
     title: 'InfinteScroll y Pull',
-    subtitle: 'Listas infinitas y pull to refres',
+    subtitle: 'Listas infinitas y pull to refresh',
     link: '/infinite',
-    icon: Icons.list_alt_rounded
+    icon: Icons.list_alt_rounded,
   ),
-
 ];
